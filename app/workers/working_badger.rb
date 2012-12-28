e3f69c99086cd9d4af72f47590c0fa5a0c6841ce
Joshua Wood <josh@hintmedia.com>
2012-12-28 12:51:36 -0800

    Remove some debugging cruft

--- app/workers/working_badger.rb
+++ app/workers/working_badger.rb
@@ -2,8 +2,6 @@ class WorkingBadger
   include Sidekiq::Worker
 
   def perform(notice)
-    puts notice.class.name.inspect
-    puts notice.inspect
     Honeybadger.sender.send_to_honeybadger(notice)
   end
 end
