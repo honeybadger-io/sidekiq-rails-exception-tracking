5ebcfa2a7bf5e7010bd1b58715358fd3204b452d
Joshua Wood <josh@hintmedia.com>
2014-06-05 09:52:32 -0700

    Enable debug

--- config/initializers/honeybadger.rb
+++ config/initializers/honeybadger.rb
@@ -1,6 +1,7 @@
 Honeybadger.configure do |config|
   config.api_key = ENV['HONEYBADGER_API_KEY']
   config.development_environments = []
+  config.debug = true
   config.async do |notice|
     WorkingBadger.perform_async(notice.to_json)
   end
